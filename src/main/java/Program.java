a020daeca904bcd54730cfa7b0286c11041e0fca
Amir <amirzny@gmail.com>
2022-03-01 13:19:53 +0200

    coupon

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -8,15 +8,15 @@ public class Program {
 
     public static void main(String[] args) {
 //        Test.testAll();
-        /*
+
         DBrunQuery.runQuery(DBmanager.CREATE_SCHEMA);
         DBrunQuery.runQuery(DBmanager.CREATE_COMPANIES_TABLE);
         DBrunQuery.runQuery(DBmanager.CREATE_COSTUMERS_TABLE);
         DBrunQuery.runQuery(DBmanager.CREATE_CATEGORIES_TABLE);
         DBrunQuery.runQuery(DBmanager.CREATE_COUPONS_TABLE);
         DBrunQuery.runQuery(DBmanager.CREATE_COSTUMERS_COUPONS_TABLE);
-*/
-        for(Category category : Category.values()){
+
+        for (Category category : Category.values()) {
             Map<Integer, Object> values = Map.of(1, category);
             DBrunQuery.runQuery(DBmanager.ADD_CATEGORY, values);
         }
