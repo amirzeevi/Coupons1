ecee3c6274581c5ccaa28931059e55df601cc3c9
amirzeevi <94979040+amirzeevi@users.noreply.github.com>
2022-05-10 13:30:05 +0300

    Update CategoriesDAO.java

--- src/main/java/dao/CategoriesDAO.java
+++ src/main/java/dao/CategoriesDAO.java
@@ -2,21 +2,11 @@ package dao;
 
 import beans.Category;
 
-/**
- * The CategoriesDAO interface is to be implemented by the class
- * that should access the database and update the categories table.
- */
 public interface CategoriesDAO {
-    /**
-     * Will add all existing categories to the categories table.
-     */
+
     void addAllCategories();
-    /**
-     * Will add the specified category only if it does not already exist in the database
-     */
+
     void addOneCategory(Category category);
-    /**
-     * Will delete the specified category from the database
-     */
+
     void deleteCategory(Category category);
 }
