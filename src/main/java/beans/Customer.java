22fc13180a3973e2d0efb418b88f4e6ec34bc52c
amirzeevi <94979040+amirzeevi@users.noreply.github.com>
2022-05-10 13:29:30 +0300

    Update Customer.java

--- src/main/java/beans/Customer.java
+++ src/main/java/beans/Customer.java
@@ -4,9 +4,6 @@ import exceptions.ValueSetNotAllowed;
 
 import java.util.List;
 
-/**
- * Customer bean to specify any costumer that should log in
- */
 public class Customer {
     private int id;
     private String firstName;
@@ -15,18 +12,12 @@ public class Customer {
     private String password;
     private List<Coupon> coupons;
 
-    /**
-     * Constructs the costumers attributes for when retrieving from database.
-     */
     public Customer(int id, String firstName, String lastName, String email, String password, List<Coupon> coupons) {
         this(firstName, lastName, email, password);
         this.id = id;
         this.coupons = coupons;
     }
 
-    /**
-     * Required constructor for when initializing a new customer.
-     */
     public Customer(String firstName, String lastName, String email, String password) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -34,96 +25,54 @@ public class Customer {
         this.password = password;
     }
 
-    /**
-     * Returns the customer id
-     */
     public int getId() {
         return id;
     }
 
-    /**
-     * Can not change customer id.
-     * will throw an exception.
-     *
-     * @throws ValueSetNotAllowed
-     */
     public void setId(int id) throws ValueSetNotAllowed {
         throw new ValueSetNotAllowed("You can not change customer id");
     }
 
-    /**
-     * Returns the customer first name
-     */
     public String getFirstName() {
         return firstName;
     }
 
-    /**
-     * Sets the customer first name
-     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
-    /**
-     * Returns the customer last name
-     */
     public String getLastName() {
         return lastName;
     }
 
-    /**
-     * Sets the customer last name
-     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
-    /**
-     * Returns the customer email
-     */
     public String getEmail() {
         return email;
     }
 
-    /**
-     * Sets the customer email
-     */
     public void setEmail(String email) {
         this.email = email;
     }
 
-    /**
-     * Returns the customer password to log in
-     */
     public String getPassword() {
         return password;
     }
-
-    /**
-     * Sets the customer password to log in to a new one
-     */
+    
     public void setPassword(String password) {
         this.password = password;
     }
 
-    /**
-     * Returns a list of all coupons purchased by customer
-     */
     public List<Coupon> getCoupons() {
         return coupons;
     }
 
-    /**
-     * Sets the list of coupons purchased by customer
-     */
     public void setCoupons(List<Coupon> coupons) {
         this.coupons = coupons;
     }
 
-    /**
-     * Overrides toString method to display customer details
-     */
     @Override
     public String toString() {
         return "Customer{" +
