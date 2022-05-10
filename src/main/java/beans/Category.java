0fe8b743ae0cb2771dec721a802c0c7a7362d3e5
amirzeevi <94979040+amirzeevi@users.noreply.github.com>
2022-05-10 13:25:44 +0300

    Update Category.java

--- src/main/java/beans/Category.java
+++ src/main/java/beans/Category.java
@@ -1,7 +1,12 @@
 package beans;
 
 public enum Category {
-    FOOD, ELECTRICITY, RESTAURANT, VACATION, HOLIDAY, MUSEUM;
+    FOOD,
+    ELECTRICITY,
+    RESTAURANT,
+    VACATION,
+    HOLIDAY,
+    MUSEUM;
 
     public final int value = ordinal() + 1;
 }
