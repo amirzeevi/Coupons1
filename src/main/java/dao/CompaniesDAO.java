f9a19171169e0cd0a8a6f51070c51ed2a4f47b87
amirzeevi <94979040+amirzeevi@users.noreply.github.com>
2022-05-10 13:30:50 +0300

    Update CompaniesDAO.java

--- src/main/java/dao/CompaniesDAO.java
+++ src/main/java/dao/CompaniesDAO.java
@@ -4,55 +4,23 @@ import beans.Company;
 
 import java.util.List;
 
-/**
- * The CompanyDAO interface is to be implemented by the class
- * that should access the database and update the company table.
- */
 public interface CompaniesDAO {
-    /**
-     * Returns from the database the company's id, based on the specified email and password.
-     * if it does not find a match, will return the id 0
-     */
+
     int getCompanyId(String email, String password);
 
-    /**
-     * Returns true if the company id exists in the database.
-     */
     boolean isCompanyExist(int companyId);
-    /**
-     * This will return true if the company exist in the database based on its email, or name.
-     */
+  
     boolean isCompanyNameOrEmailExist(Company company);
 
-    /**
-     * When updating a company's name we need to make sure the name does not already exist.
-     * Will return true if it finds another company with the same name.
-     */
     boolean updateCompanyIsEmailExist(Company company);
 
-    /**
-     * Adds the specified company to the companies table.
-     */
     void addCompany(Company company);
 
-    /**
-     * Updates the specified company in the companies table.
-     */
     void updateCompany(Company company);
 
-    /**
-     * Deletes the specified company from the companies table, also
-     * will delete any coupon that the company owns
-     */
     void deleteCompany(int companyID);
 
-    /**
-     * Returns a list of all companies from the database.
-     */
     List<Company> getAllCompanies();
-
-    /**
-     * Returns the specified company from the database.
-     */
+    
     Company getOneCompany(int companyID);
 }
