889e8793d0b27374ac4ed09ee6b01971b0f9387a
amirzeevi <94979040+amirzeevi@users.noreply.github.com>
2022-05-10 13:24:47 +0300

    Update Category.java

--- src/main/java/beans/Category.java
+++ src/main/java/beans/Category.java
@@ -1,8 +1,5 @@
 package beans;
 
-/**
- * An enum to describe different types of coupons to be used in the coupon system.
- */
 public enum Category {
     FOOD, ELECTRICITY, RESTAURANT, VACATION, HOLIDAY, MUSEUM;
 
